725379ded9f72645eefabc870d5322b85d58f0ac
Shivansh Soni <shivanshsoni568@gmail.com>
2024-08-28 10:00:05 +0530

    Single Responsibility Principle (SRP)

---
+++ src/com/shivansh/single_responsibility_principle/EmailService.java
@@ -0,0 +1,10 @@
+package com.shivansh.single_responsibility_principle;
+
+public class EmailService {
+    public void sendEmail(String email, String message) {
+        // Code to send email
+    }
+}
+
+
+// This class is only responsible for managing Email Service
---
+++ src/com/shivansh/single_responsibility_principle/User.java
@@ -0,0 +1,25 @@
+package com.shivansh.single_responsibility_principle;
+
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
+// This class is only responsible for managing User's data
